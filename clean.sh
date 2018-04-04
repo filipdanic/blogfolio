3423313fb876c49e4cfd506d310ef66dfbb37ca5
Filip Danić <filip@spicefactory.co>
2018-04-04 11:49:29 +0200

    update clean.sh script

--- clean.sh
+++ clean.sh
@@ -1,1 +1,10 @@
-rm -rf 404 && rm -rf 2017 && rm -rf about && rm -rf open-source-software && rm -rf content && rm -rf css && rm -rf speaking && rm -rf writing && rm index.html
+#!/usr/bin/env bash
+rm -rf 404 && \
+rm -rf 2017 && \
+rm -rf about && \
+rm -rf open-source-software && \
+rm -rf content && \
+rm -rf css && \
+rm -rf speaking && \
+rm -rf writing && \
+rm index.html
